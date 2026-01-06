9a7636972882a24d79fe3f596f9f5cf9870c467a
bn256 <116483234+blancnoir256@users.noreply.github.com>
2026-01-06 22:03:43 +0900

    add コメント

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,6 +9,7 @@ type ServersCommand struct {
 	Commands map[string]command
 }
 
+// CLIにコマンドを登録
 func Compile() (*ServersCommand, error) {
 	cmd := &ServersCommand{}
 	cmd.Commands = make(map[string]command)
