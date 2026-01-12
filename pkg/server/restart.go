4fa048be775d96c588c0753dea4e10d188157556
bn256 <116483234+blancnoir256@users.noreply.github.com>
2026-01-12 22:42:22 +0900

    feat: restartCommandの引数を修正し、Sakura APIへのリクエストを更新

--- pkg/server/restart.go
+++ pkg/server/restart.go
@@ -5,9 +5,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/dghubble/sling"
-	"github.com/samber/lo"
 	"github.com/traPtitech/sakura-DevOpsBot/pkg/config"
 )
 
@@ -17,35 +17,19 @@ type restartCommand struct {
 type m map[string]any
 
 func (sc *restartCommand) Execute(args []string) error {
-	if len(args) < 1 {
+	if len(args) != 1 {
 		return fmt.Errorf("invalid arguments, expected server id")
 	}
 
-	serverID := args[0]
-	args = args[1:]
-
-	if len(args) < 1 {
-		return fmt.Errorf("invalid arguments, expected restart type (SOFT or HARD)")
-	}
-
-	// args == [SOFT|HARD]
-	restartType := args[0]
-
-	if !lo.Contains([]string{"SOFT", "HARD"}, restartType) {
-		return fmt.Errorf("unknown restart type: %s", restartType)
-	}
-
-	token, err := getConohaAPIToken()
+	serverID, err := strconv.Atoi(args[0])
 	if err != nil {
-		return fmt.Errorf("failed to get conoha api token: %w", err)
+		return fmt.Errorf("invalid arguments, server id is must be integer")
 	}
 
 	req, err := sling.New().
-		Base(config.C.Servers.Conoha.Origin.Compute).
-		Post(fmt.Sprintf("v2/%s/servers/%s/action", config.C.Servers.Conoha.TenantID, serverID)).
-		BodyJSON(m{"reboot": m{"type": args[0]}}).
-		Set("Accept", "application/json").
-		Set("X-Auth-Token", token).
+		Base(config.C.Servers.Sakura.Origin).
+		Post(fmt.Sprintf("%s/%d/force-reboot", config.C.Servers.Sakura.ServersApiURLPath, serverID)).
+		Add("Authorization", "Bearer "+config.C.Servers.Sakura.BearerToken).
 		Request()
 	if err != nil {
 		return fmt.Errorf("failed to create restart request: %w", err)
@@ -65,13 +49,12 @@ func (sc *restartCommand) Execute(args []string) error {
 
 	logStr := fmt.Sprintf(`Request
 - URL: %s
-- RestartType: %s
 
 Response
 - Header: %+v
 - Body: %s
 - Status: %s (Expected: 202)
-`, req.URL.String(), restartType, resp.Header, string(respBody), resp.Status)
+`, req.URL.String(), resp.Header, string(respBody), resp.Status)
 	log.Println(logStr)
 
 	if resp.StatusCode != http.StatusAccepted {
