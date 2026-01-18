a1408c37580f938e0e1a93966a20dbedbb642791
blancnoir256 <116483234+blancnoir256@users.noreply.github.com>
2026-01-18 16:08:09 +0900

    feat: ログ出力にCPUコア数を追加し、フォーマットを修正

--- pkg/server/hosts.go
+++ pkg/server/hosts.go
@@ -139,11 +139,11 @@ func (sc *hostsCommand) Execute(args []string) error {
 		return servers[i].Name < servers[j].Name
 	})
 
-	log.Printf("Server Name :\t Server ID, \t Zone Name, \t ipv4 address, \t ipv6 address, \t Memory size, \t Storage size(Storage type)")
+	log.Printf("Server Name :\t Server ID, \t Zone Name, \t cpu cores, \t ipv4 address, \t ipv6 address, \t Memory size, \t Storage size(Storage type)")
 	for _, server := range servers {
-		logMsg := fmt.Sprintf("%s :\t %s, \t %s, \t %s, \t %s, \t %sMiB", server.Name, server.ID, server.Zone, server.Ipv4, server.Ipv6, server.MemoryMiB)
+		logMsg := fmt.Sprintf("%s :\t %d, \t %s, \t %d, \t %s, \t %s, \t %dMiB", server.Name, server.ID, server.Zone, server.CpuCores, server.Ipv4, server.Ipv6, server.MemoryMiB)
 		for _, storage := range server.Storage {
-			logMsg += fmt.Sprintf(", \t %sGiB(%s)", storage.Size, storage.Type)
+			logMsg += fmt.Sprintf(", \t %dGiB(%s)", storage.Size, storage.Type)
 		}
 		log.Printf(logMsg)
 	}
